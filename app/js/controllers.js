67a95d4c8e57516b8de79bc24b49f0cd82aa675f
nchykina <nchykina@natallias-air>
2016-06-22 00:28:02 +0400

    resources

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4150,6 +4150,7 @@ function stripeCtrl($scope, $http, $state) {
                                     .success(function (response) {
                                         if (response.success) {
                                             console.log("Subscriptions created");
+                                            $state.go('commerce.orders');
                                         } else {
                                             console.log("Error, subscriptions not created");
                                         }
