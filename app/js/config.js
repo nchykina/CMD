fd2dc7a459c228639dcfefe3ea34a2c20fc6ec70
nchykina <nchykina@natallias-air>
2016-06-27 00:41:37 +0400

    logo click

--- app/js/config.js
+++ app/js/config.js
@@ -235,6 +235,61 @@ function config($stateProvider, $locationProvider, $urlRouterProvider, $ocLazyLo
                 //params: { job: {} },
                 data: {pageTitle: 'DNA resequencing pipeline: Step 3'}
             })
+            
+            
+            .state('pipelines.rna_reseq_home', {
+                url: "/rna_resequencing/home",
+                controller: 'rnaReseqHomeController',
+                controllerAs: 'homec',
+                templateUrl: "views/pipelines/rna_reseq/home.html",
+                data: {pageTitle: 'RNA resequencing'}
+            })
+            .state('pipelines.rna_reseq_new', {
+                url: "/rna_resequencing/new",
+                templateUrl: "views/pipelines/rna_reseq/new.html",
+                //templateUrl: "views/debug/parent_state.html",
+                controller: 'rnaReseqNewController',
+                controllerAs: 'newc',
+                params: {job: {}},
+                data: {pageTitle: 'RNA resequencing'},
+                resolve: {
+                    loadPlugin: function ($ocLazyLoad) {
+                        return $ocLazyLoad.load([
+                            {
+                                files: ['css/plugins/steps/jquery.steps.css']
+                            }
+                        ]);
+                    }
+                }
+            })
+            .state('pipelines.rna_reseq_new.step1', {
+                url: '/step1',
+                templateUrl: "views/pipelines/rna_reseq/step1.html",
+                //templateUrl: "views/debug/normal_state.html",
+                controller: 'rnaReseqNewController',
+                controllerAs: 'newc',
+                //params: { job: {} },
+                data: {pageTitle: 'RNA resequencing pipeline: Step 1'}
+            })
+            .state('pipelines.rna_reseq_new.step2', {
+                url: '/step2',
+                templateUrl: "views/pipelines/rna_reseq/step2.html",
+                controller: 'rnaReseqNewController',
+                controllerAs: 'newc',
+                //params: { job: {} },
+                data: {pageTitle: 'RNA resequencing pipeline: Step 2'}
+            })
+            .state('pipelines.rna_reseq_new.step3', {
+                url: '/step3',
+                templateUrl: "views/pipelines/rna_reseq/step3.html",
+                controller: 'rnaReseqNewController',
+                controllerAs: 'newc',
+                //params: { job: {} },
+                data: {pageTitle: 'RNA resequencing pipeline: Step 3'}
+            })
+            
+            
+            
             .state('charts', {
                 abstract: true,
                 url: "/charts",
